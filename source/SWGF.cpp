84f9d0bb329acb540d4f79f73370f7afe2d3d87d
Popov Evgeniy <tuzik87@inbox.ru>
2020-12-09 01:09:50 +0700

    Source code was synced with Neon game development kit 4.9.6

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -1460,7 +1460,7 @@ wchar_t *Multimedia::get_memory(const size_t length)
  target=NULL;
  try
  {
-  target=new wchar_t[length];
+  target=new wchar_t[length+1];
  }
  catch (...)
  {
@@ -1471,8 +1471,9 @@ wchar_t *Multimedia::get_memory(const size_t length)
 
 void Multimedia::clear_buffer(wchar_t *target,const size_t length)
 {
- size_t index;
- for (index=0;index<length;++index)
+ size_t index,stop;
+ stop=length+1;
+ for (index=0;index<stop;++index)
  {
   target[index]=0;
  }
@@ -2057,7 +2058,7 @@ void Image::load_tga(const char *name)
    }
 
   }
-  free(compressed);
+  delete[] compressed;
  }
  target.close();
  data=uncompressed;
@@ -2110,7 +2111,7 @@ void Image::load_pcx(const char *name)
   }
 
  }
- free(original);
+ delete[] original;
  original=this->create_buffer(uncompressed_length);
  for (x=0;x<width;++x)
  {
@@ -2124,7 +2125,7 @@ void Image::load_pcx(const char *name)
   }
 
  }
- free(uncompressed);
+ delete[] uncompressed;
  data=original;
 }
 
