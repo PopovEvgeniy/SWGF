798eec835b01a795a2b34fac4cd2ad1b3723cb2f
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2023-10-27 00:38:00 +0700

    Source code synced with Eugene game development kit 7.6.6

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -2083,6 +2083,16 @@ namespace SWGF
    return this->get_display_height();
   }
 
+  bool Screen::check_x(const unsigned int x) const
+  {
+   return x<this->get_width();
+  }
+
+  bool Screen::check_y(const unsigned int y) const
+  {
+   return y<this->get_height();
+  }
+
   Screen* Screen::get_handle()
   {
    return this;
