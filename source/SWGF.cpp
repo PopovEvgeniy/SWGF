9227adf2c048e7a989b7a653c319e22d843b31a3
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2023-12-01 21:17:53 +0300

    Source code synced with Eugene game development kit 7.7

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -576,6 +576,8 @@ namespace SWGF
    image.set_length(0);
    source_width=0;
    source_height=0;
+   x_ratio=0;
+   y_ratio=0;
    target_width=1;
    target_height=1;
   }
@@ -592,12 +594,12 @@ namespace SWGF
 
   unsigned int Resizer::get_source_x(const unsigned int x) const
   {
-   return (x*(source_width-1)+1)/(target_width-1);
+   return (x*x_ratio+1)/UCHAR_MAX;
   }
 
   unsigned int Resizer::get_source_y(const unsigned int y) const
   {
-   return (y*(source_height-1)+1)/(target_height-1);
+   return (y*y_ratio+1)/UCHAR_MAX;
   }
 
   unsigned int Resizer::get_next_x(const unsigned int x) const
@@ -719,6 +721,12 @@ namespace SWGF
 
   }
 
+  void Resizer::calculate_ratio()
+  {
+   x_ratio=(source_width*UCHAR_MAX)/target_width;
+   y_ratio=(source_height*UCHAR_MAX)/target_height;
+  }
+
   void Resizer::calculate_size()
   {
    while (target_width<source_width)
@@ -745,6 +753,7 @@ namespace SWGF
    this->set_setting(width,height);
    this->calculate_size();
    this->correct_size(limit);
+   this->calculate_ratio();
    this->create_texture();
    this->resize_image(target);
   }
