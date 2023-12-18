e3b56be78254736d212abb1b76a0394c458c3eda
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2023-12-18 19:05:47 +0300

    Source code synced with Eugene game development kit 7.8.1

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -3531,6 +3531,30 @@ namespace SWGF
    return cartesian_y;
   }
 
+  int Isometric::get_isometric_x(const int x,const int y)
+  {
+   this->set_target(x,y);
+   return this->get_isometric_x();
+  }
+
+  int Isometric::get_isometric_y(const int x,const int y)
+  {
+   this->set_target(x,y);
+   return this->get_isometric_y();
+  }
+
+  int Isometric::get_cartesian_x(const int x,const int y)
+  {
+   this->set_target(x,y);
+   return this->get_cartesian_x();
+  }
+
+  int Isometric::get_cartesian_y(const int x,const int y)
+  {
+   this->set_target(x,y);
+   return this->get_cartesian_y();
+  }
+
   World::World()
   {
    half_tile_width=1;
