6b18278f2bceaba56af3d60ba44d833dd8674fc7
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-01-10 20:22:01 +0300

    Source code synced with Eugene game development kit 7.9.2

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -3143,6 +3143,11 @@ namespace SWGF
    stage.destroy();
   }
 
+  Background* Background::get_handle()
+  {
+   return this;
+  }
+
   void Background::prepare(const Screen *screen)
   {
    if (screen!=NULL)
@@ -3246,6 +3251,11 @@ namespace SWGF
   void Background::destroy()
   {
    stage.destroy();
+  }
+
+  bool Background::is_last_frame() const
+  {
+   return stage.is_last_frame();
   }
 
   unsigned int Background::get_frame() const
--- source/SWGF.h
+++ source/SWGF.h
@@ -896,6 +896,7 @@ typedef enum
    public:
    Background();
    ~Background();
+   Background* get_handle();
    void prepare(const Screen *screen);
    void prepare(Screen &screen);
    void prepare(const unsigned int width,const unsigned int height);
@@ -916,6 +917,7 @@ typedef enum
    void draw(const unsigned int target);
    void destroy_image();
    void destroy();
+   bool is_last_frame() const;
    unsigned int get_frame() const;
    unsigned int get_frames() const;
    unsigned int get_width() const;
