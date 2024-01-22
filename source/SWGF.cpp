eb2cb48c3a13f441854eda87df0ad9e3984d6e87
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-01-22 21:03:34 +0300

    Source code synced with Eugene game development kit 7.9.7

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -2754,6 +2754,16 @@ namespace SWGF
    this->set_position(start_x,start_y);
   }
 
+  void Billboard::go_start_x()
+  {
+   this->set_x(start_x);
+  }
+
+  void Billboard::go_start_y()
+  {
+   this->set_y(start_y);
+  }
+
   void Billboard::draw()
   {
    this->check_transparent();
