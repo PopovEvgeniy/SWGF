f0bed784c05930b496f7e5a29311a5e001014c3a
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-02-15 22:01:07 +0700

    Source code synced with Eugene game development kit 8.2.3

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -526,14 +526,14 @@ namespace SWGF
  namespace Core
  {
 
-  double get_start_offset(const double current,const double total)
+  float get_start_offset(const float current,const float total)
   {
-   return (1.0/total)*(current-1.0);
+   return (1.0f/total)*(current-1.0f);
   }
 
-  double get_end_offset(const double current,const double total)
+  float get_end_offset(const float current,const float total)
   {
-   return (1.0/total)*current;
+   return (1.0f/total)*current;
   }
 
   unsigned int get_pixel_component(const unsigned int pixel,const Core::PIXEL_COMPONENT component)
@@ -798,14 +798,14 @@ namespace SWGF
    vertex[2].y=0;
    vertex[3].x=0;
    vertex[3].y=0;
-   point[0].u=0.0;
-   point[0].v=1.0;
-   point[1].u=1.0;
-   point[1].v=1.0;
-   point[2].u=1.0;
-   point[2].v=0.0;
-   point[3].u=0.0;
-   point[3].v=0.0;
+   point[0].u=0.0f;
+   point[0].v=1.0f;
+   point[1].u=1.0f;
+   point[1].v=1.0f;
+   point[2].u=1.0f;
+   point[2].v=0.0f;
+   point[3].u=0.0f;
+   point[3].v=0.0f;
   }
 
   Shape::~Shape()
@@ -889,7 +889,7 @@ namespace SWGF
    current_y=y;
   }
 
-  void Shape::set_tile_offset(const double row,const double rows,const double column,const double columns)
+  void Shape::set_tile_offset(const float row,const float rows,const float column,const float columns)
   {
    point[0].u=Core::get_start_offset(row,rows);
    point[0].v=Core::get_end_offset(column,columns);
@@ -901,14 +901,14 @@ namespace SWGF
    point[3].v=Core::get_start_offset(column,columns);
   }
 
-  void Shape::set_horizontal_offset(const double current,const double total)
+  void Shape::set_horizontal_offset(const float current,const float total)
   {
-   this->set_tile_offset(current,total,1.0,1.0);
+   this->set_tile_offset(current,total,1.0f,1.0f);
   }
 
-  void Shape::set_vertical_offset(const double current,const double total)
+  void Shape::set_vertical_offset(const float current,const float total)
   {
-   this->set_tile_offset(1.0,1.0,current,total);
+   this->set_tile_offset(1.0f,1.0f,current,total);
   }
 
   Rectangle::Rectangle()
@@ -968,7 +968,7 @@ namespace SWGF
   void Rectangle::load_data()
   {
    glVertexPointer(2,GL_INT,0,vertex);
-   glTexCoordPointer(2,GL_DOUBLE,0,point);
+   glTexCoordPointer(2,GL_FLOAT,0,point);
   }
 
   void Rectangle::draw_rectangle()
@@ -2838,11 +2838,11 @@ namespace SWGF
   {
    if (current_kind==SWGF::HORIZONTAL_ANIMATED)
    {
-    billboard.set_horizontal_offset(static_cast<double>(this->get_frame()),static_cast<double>(this->get_frames()));
+    billboard.set_horizontal_offset(static_cast<float>(this->get_frame()),static_cast<float>(this->get_frames()));
    }
    else
    {
-    billboard.set_vertical_offset(static_cast<double>(this->get_frame()),static_cast<double>(this->get_frames()));
+    billboard.set_vertical_offset(static_cast<float>(this->get_frame()),static_cast<float>(this->get_frames()));
    }
 
   }
@@ -3152,7 +3152,7 @@ namespace SWGF
    {
     if (this->check_column(column)==true)
     {
-     billboard.set_tile_offset(static_cast<double>(row),static_cast<double>(rows),static_cast<double>(column),static_cast<double>(columns));
+     billboard.set_tile_offset(static_cast<float>(row),static_cast<float>(rows),static_cast<float>(column),static_cast<float>(columns));
     }
 
    }
