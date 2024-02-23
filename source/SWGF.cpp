62f05717095240ebc0efea7fda6af9a626c94661
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-02-23 21:38:48 +0700

    Source code synced with Eugene game development kit 8.3.4

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -3321,6 +3321,11 @@ namespace SWGF
    return stage.is_last_frame();
   }
 
+  bool Background::check_frame(const unsigned int target) const
+  {
+   return stage.check_frame(target);
+  }
+
   unsigned int Background::get_frame() const
   {
    return stage.get_frame();
