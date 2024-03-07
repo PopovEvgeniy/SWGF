763b58ea82340a883f7ca4c8d142754b36a504bb
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-03-07 23:42:43 +0700

    Source code synced with Neon game development kit 7.3.3

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -457,11 +457,6 @@ namespace SWGF
 
   }
 
-  bool WINGL::check_flag(const unsigned long int flag) const
-  {
-   return (setting.dwFlags&flag)!=0;
-  }
-
   void WINGL::set_pixel_format(HDC target)
   {
    int format;
@@ -521,7 +516,7 @@ namespace SWGF
 
   bool WINGL::is_software_render() const
   {
-   return (this->check_flag(PFD_GENERIC_FORMAT)==true) && (this->check_flag(PFD_GENERIC_ACCELERATED)==false);
+   return ((setting.dwFlags&PFD_GENERIC_FORMAT)!=0) && ((setting.dwFlags&PFD_GENERIC_ACCELERATED)==0);
   }
 
  }
