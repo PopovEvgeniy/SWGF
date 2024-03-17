8bc89bcc774a0ae8f65119f489e354700a4f1bcf
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-03-17 20:25:40 +0700

    Source code synced with Eugene game development kit 8.4.1

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -367,7 +367,7 @@ namespace SWGF
 
   void Engine::create_window()
   {
-   window=CreateWindowEx(WS_EX_LEFT,window_class.lpszClassName,NULL,WS_VISIBLE|WS_POPUP,0,0,GetSystemMetrics(SM_CXSCREEN),GetSystemMetrics(SM_CYSCREEN),NULL,NULL,window_class.hInstance,NULL);
+   window=CreateWindowEx(WS_EX_APPWINDOW,window_class.lpszClassName,NULL,WS_VISIBLE|WS_POPUP,0,0,GetSystemMetrics(SM_CXSCREEN),GetSystemMetrics(SM_CYSCREEN),NULL,NULL,window_class.hInstance,NULL);
    if (window==NULL)
    {
     SWGF::Halt("Can't create window");
