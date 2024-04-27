b25d10e97b701a7c1ae246f02935024e3422205a
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-04-27 20:02:41 +0700

    Source code synced with Eugene game development kit 8.6.1

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -2242,15 +2242,6 @@ namespace SWGF
    original.destroy_buffer();
   }
 
-  void Image::flip_image(const Core::MIRROR_KIND mirror)
-  {
-   if (data.get_buffer()!=NULL)
-   {
-    this->mirror_image(mirror);
-   }
-
-  }
-
   void Image::uncompress_tga_data(const unsigned char *target)
   {
    size_t index,position,amount;
@@ -2375,21 +2366,6 @@ namespace SWGF
   Image* Image::get_handle()
   {
    return this;
-  }
-
-  void Image::horizontal_mirror()
-  {
-   this->flip_image(Core::HORIZONTAL_MIRROR);
-  }
-
-  void Image::vertical_mirror()
-  {
-   this->flip_image(Core::VERTICAL_MIRROR);
-  }
-
-  void Image::complex_mirror()
-  {
-   this->flip_image(Core::MIRROR_BOTH);
   }
 
   void Image::destroy_image()
