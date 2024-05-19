84764e1d6fcface1361a3cf9cd612ffd7461c10d
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-05-20 02:28:26 +0700

    Source code synced with Eugene game development kit 8.7.3

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -292,11 +292,6 @@ namespace SWGF
     CloseWindow(window);
     window=NULL;
    }
-   if (window_class.hbrBackground!=NULL)
-   {
-    DeleteObject(window_class.hbrBackground);
-    window_class.hbrBackground=NULL;
-   }
    UnregisterClass(window_class.lpszClassName,window_class.hInstance);
   }
 
