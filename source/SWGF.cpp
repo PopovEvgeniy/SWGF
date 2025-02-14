65b236d4fe7c41be8f9d495337e1799785e58615
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-02-15 00:58:45 +0700

    The source code synced with the Eugene game development kit 9.5.3

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -1025,6 +1025,7 @@ namespace SWGF
    glDisable(GL_COLOR_LOGIC_OP);
    glDisable(GL_FOG);
    glDisable(GL_STENCIL_TEST);
+   glDisable(GL_SCISSOR_TEST);
    glDisable(GL_LIGHTING);
    glDisable(GL_NORMALIZE);
    glDisable(GL_AUTO_NORMAL);
@@ -1053,7 +1054,6 @@ namespace SWGF
    glDisable(GL_MAP2_TEXTURE_COORD_4);
    glDisable(GL_MAP2_VERTEX_3);
    glDisable(GL_MAP2_VERTEX_4);
-   glEnable(GL_SCISSOR_TEST);
    glEnable(GL_TEXTURE_2D);
    glEnable(GL_ALPHA_TEST);
    glEnable(GL_BLEND);
@@ -1105,7 +1105,6 @@ namespace SWGF
    glMatrixMode(GL_PROJECTION);
    glLoadIdentity();
    glOrtho(0.0,static_cast<double>(width),static_cast<double>(height),0.0,0.0,1.0);
-   glScissor(0,0,width,height);
    glViewport(0,0,width,height);
   }
 
