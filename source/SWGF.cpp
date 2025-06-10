40f443baefcdb4c2fdbb8ec04f4909273895fc3b
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-06-10 21:43:31 +0700

    The source code synced with the Black game development kit 7.5.1

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -2276,16 +2276,6 @@ namespace SWGF
    return y_offset+viewport_height;
   }
 
-  unsigned int Camera::get_lowest_x() const
-  {
-   return this->get_highest_x()-viewport_width;
-  }
-
-  unsigned int Camera::get_lowest_y() const
-  {
-   return this->get_highest_y()-viewport_height;
-  }
-
   unsigned int Camera::get_highest_x_offset() const
   {
    return highest_x_offset;
