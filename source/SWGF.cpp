d4f7c116e8cbe439f04048d6a1ff12ec13e95db7
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-11-05 22:30:49 +0700

    The source code synced with the Eugene Game Development Kit 9.8.4

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -3928,6 +3928,21 @@ namespace SWGF
    stage.complex_mirror();
   }
 
+  bool Parallax::is_horizontally_mirrored() const
+  {
+   return stage.is_horizontally_mirrored();
+  }
+
+  bool Parallax::is_vertically_mirrored() const
+  {
+   return stage.is_vertically_mirrored();
+  }
+
+  bool Parallax::is_complex_mirrored() const
+  {
+   return stage.is_complex_mirrored();
+  }
+
   void Parallax::destroy_image()
   {
    stage.destroy_image();
