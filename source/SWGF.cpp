c52e8097bea0594997bdcefa4e1807b30d258cbd
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-11-07 02:21:07 +0700

    The source code synced with the Eugene Game Development Kit 9.8.5

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -3117,6 +3117,11 @@ namespace SWGF
    return mirror==Core::MIRROR_BOTH;
   }
 
+  bool Billboard::is_mirrored() const
+  {
+   return mirror!=Core::MIRROR_NONE;
+  }
+
   void Billboard::go_start()
   {
    this->set_position(start_x,start_y);
