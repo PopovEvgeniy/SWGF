f9fca1b00b3c106fb548259fc21c1f75cb04a1d2
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-11-10 22:58:33 +0700

    The source code synced with the Eugene Game Development Kit 9.8.9

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -3645,6 +3645,11 @@ namespace SWGF
    return stage.is_complex_mirrored();
   }
 
+  bool Background::is_mirrored() const
+  {
+   return stage.is_mirrored();
+  }
+
   unsigned int Background::set_target(const unsigned int target)
   {
    return stage.set_target(target);
