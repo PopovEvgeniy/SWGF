059e21ce0f6cb63960d16db986c9c193cf32eb6b
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2026-02-22 20:54:23 +0700

    The source code synced with the Eugene Game Development Kit 10.3.2

--- source/SWGF.cpp
+++ source/SWGF.cpp
@@ -270,11 +270,11 @@ namespace SWGF
     if (context!=NULL)
     {
      ReleaseDC(window,context);
+     context=NULL;
     }
     DestroyWindow(window);
+    window=NULL;
    }
-   window=NULL;
-   context=NULL;
    UnregisterClass(window_class.lpszClassName,window_class.hInstance);
   }
 
